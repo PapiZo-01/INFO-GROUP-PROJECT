846fa58c545f77bf287b76fc576ffcfe000b002c
PapiZo <naomisookoo.01@gmail.com>
2026-04-06 18:13:05 -0400

    Initialize Firebase in app.js
    Added Firebase initialization with configuration details.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log ("App loaded");
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAXkoQAjNha2j_kvmP-nQrbh4PiaakpEg8",
+    authDomain: "info-group-project-d99f5.firebaseapp.com",
+    projectId: "info-group-project-d99f5",
+    storageBucket: "info-group-project-d99f5.firebasestorage.app",
+    messagingSenderId: "62710163494",
+    appId: "1:62710163494:web:1d55f732098783ac0b5ee7"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
